0eb098c07574b89aef104ccdf4eaa21c10834861
Calota Alexandru <calotaalexandrug@gmail.com>
2019-06-08 14:53:59 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         String parola= input.nextLine();
-        Pattern p=Pattern.compile("[a-z][A-Z][0-9]");
-        Matcher m=p.matcher(parola);
-        if(m.find()&&parola.length()>=6){
+        Pattern p1=Pattern.compile("[a-z]");
+        Pattern p2=Pattern.compile("[A-Z]");
+        Pattern p3=Pattern.compile(("[0-1]"));
+        Matcher m1=p1.matcher(parola);
+        Matcher m2=p2.matcher(parola);
+        Matcher m3=p3.matcher(parola);
+
+        if(m1.find() && m2.find() && m3.find() && parola.length()>=6){
             System.out.println("Parola valida!");
         } else {
             System.out.println("Parola invalida!");
